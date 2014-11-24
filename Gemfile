f571c9eb93459bd96159a8dbd7e9aa8bb8572ca6
Jeffrey Pratt <@clementi1832>
2014-11-24 14:56:58 -0700

    Use Foreman; update source of Gemfile

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
-source :rubygems
+source 'https://rubygems.org'
+
 gem 'sinatra', '1.3.3'
 gem 'haml', '3.1.7'
 gem 'maruku', '0.6.1'
--- provision.sh
+++ provision.sh
@@ -2,6 +2,7 @@
 
 sudo apt-get install htop -y
 
+sudo gem install foreman
 sudo gem install bundler
 
 echo "export PORT=9000" >> /home/vagrant/.bashrc
