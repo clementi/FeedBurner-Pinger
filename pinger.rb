db91ffba648eca5293dc7ab2c308b187c8e44329
Jeff Pratt <me@jeffreypratt.net>
2015-03-14 13:08:33 -0600

    Call XMLRPC endpoint instead

--- pinger.rb
+++ pinger.rb
@@ -3,40 +3,77 @@ require 'haml'
 require 'json'
 require 'net/http'
 require 'sinatra'
+require 'uri'
 require File.join(File.dirname(__FILE__), 'environment')
 
 get '/' do
-  log request, params
   haml :index
 end
 
 post '/' do
-  log request, params
-  content_type :json  
+  content_type :xml
 
-  response = Net::HTTP.get URI('http://feedburner.google.com/fb/a/pingSubmit?bloglink=' + CGI::escape(params[:url]))
+  endpoint = URI.parse('http://ping.feedburner.com')
 
-  throttled_result = /Ping is throttled/.match(response)
-  if throttled_result then
-    status 200
-    return { :status => "THROTTLED", :message => throttled_result }.to_json
-  end
+  feed_url = params[:url]
+  feed_name = get_feed_name(feed_url)
 
-  success_result = /Successfully pinged/.match(response)
-  if success_result then
-    status 200
-    return { :status => "SUCCEEDED", :message => success_result }.to_json
-  end
+  request_body = build_request_bodyfeed_name, feed_url)
 
-  error_result = /Your Ping resulted in an Error/.match(response)
-  status 500
-  if error_result then
-    return { :status => "FAILED", :message => error_result }.to_json
-  else
-    return { :status => "FAILED", :message => "An unknown error occurred" }.to_json
-  end
+  request = Net::HTTP::Post.new(endpoint.request_uri)
+  request.body = request_body
+  request.content_type = 'text/xml'
+
+  response = Net::HTTP.new(endpoint.host, endpoint.port).start { |http| http.request(request) }
+
+  response.body
+
+  # TODO: Convert XML response to existing JSON response
+
+  # response = Net::HTTP.get URI('http://feedburner.google.com/fb/a/pingSubmit?bloglink=' + CGI::escape(params[:url]))
+
+  
+
+  # throttled_result = /Ping is throttled/.match(response)
+  # if throttled_result then
+  #   status 200
+  #   return { :status => "THROTTLED", :message => throttled_result }.to_json
+  # end
+
+  # success_result = /Successfully pinged/.match(response)
+  # if success_result then
+  #   status 200
+  #   return { :status => "SUCCEEDED", :message => success_result }.to_json
+  # end
+
+  # error_result = /Your Ping resulted in an Error/.match(response)
+  # status 500
+  # if error_result then
+  #   return { :status => "FAILED", :message => error_result }.to_json
+  # else
+  #   return { :status => "FAILED", :message => "An unknown error occurred" }.to_json
+  # end
 end
 
-def log(request, params)
-  puts "#{request.request_method} #{request.path_info} #{request.ip} #{params}"
+def build_request_body(feed_name, feed_url)
+  %Q(<?xml version="1.0" encoding="iso-8859-1"?>
+<methodCall>
+  <methodName>weblogUpdates.ping</methodName>
+  <params>
+    <param>
+      <value>
+        <string>#{feed_name}</string>
+      </value>
+    </param>
+    <param>
+      <value>
+        <string>#{feed_url}</string>
+      </value>
+    </param>
+  </params>
+</methodCall>)
+end
+
+def get_feed_name(url)
+  url.split('/').last
 end
